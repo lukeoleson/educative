9c6a820fbc4ba9fa4c38713253b4fc6d30772473
Luke Oleson <luke.oleson@dapperlabs.com>
2022-07-10 05:46:58 -0700

    learn node-js - fundamentals

---
+++ learn-node-js/README.md
@@ -0,0 +1,3 @@
+# Notes
+
+https://whimsical.com/node-js-7VvSk97xTKy4FTncN6tYe9
---
+++ learn-node-js/fundamentals/input.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let query = 'What is your name?\n'
+
+rl.question(query, (answer) => {
+  console.log(`Hello ${answer}!`);
+  
+  rl.close();
+});
